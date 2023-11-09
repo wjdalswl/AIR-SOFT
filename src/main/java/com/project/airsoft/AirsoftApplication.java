26b15cefe26946030a72a4a501740d415f05fa63
koreanair777 <jinuklee777@soongsil.ac.kr>
2023-11-10 01:55:09 +0900

    first commit

---
+++ src/main/java/com/project/airsoft/AirsoftApplication.java
@@ -0,0 +1,13 @@
+package com.project.airsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirsoftApplication.class, args);
+	}
+
+}
