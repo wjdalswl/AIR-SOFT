f5951cf7f003f0d7b446fed3931b6941f2a10873
koreanair777 <jinuklee777@soongsil.ac.kr>
2023-11-10 02:08:27 +0900

    frontend backend setting axois

--- src/main/java/com/project/airsoft/controller/TestController.java
+++ src/main/java/com/project/airsoft/controller/TestController.java
@@ -1,8 +1,17 @@
 package com.project.airsoft.controller;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        log.info("테스트");
+        return "Hello, World";
+    }
 }
